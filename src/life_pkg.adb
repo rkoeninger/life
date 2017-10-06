af11f76f3eb6859de61c3f5a3b86e1aa3248dd2a
Robert Koeninger <rkoeninger@att.net>
2017-10-06 17:10:39 -0400

    Added Life_Pkg

---
+++ src/life_pkg.adb
@@ -0,0 +1,23 @@
+package body Life_Pkg is
+
+   -------------
+   -- Gtk_New --
+   -------------
+
+   procedure Gtk_New (Life_Value : out Life_Access) is
+   begin
+      --  Generated stub: replace with real body!
+      pragma Compile_Time_Warning (Standard.True, "Gtk_New unimplemented");
+   end;
+
+   ----------------
+   -- Initialize --
+   ----------------
+
+   procedure Initialize (Life_Value : out Life_Record'Class) is
+   begin
+      --  Generated stub: replace with real body!
+      pragma Compile_Time_Warning (Standard.True, "Initialize unimplemented");
+   end;
+
+end;
--- src/main.adb
+++ src/main.adb
@@ -1,6 +1,9 @@
 with Ada.Text_IO; use Ada.Text_IO;
-with World;       use World;
 with Gtk;         use Gtk;
+with Gtk.Main;
+with Gtk.Widget;  use Gtk.Widget;
+with Life_Pkg;    use Life_Pkg;
+with World;       use World;
 
 procedure Main is
    WG : World_Grid (16);
@@ -10,4 +13,7 @@ begin
    B := Get_Spot (WG, 1, 1);
    Put_Line ("World Size: " & Integer'Image(WG.Size));
    Put_Line ("Hello Ada!");
+   --Gtk.Main.Init;
+   --Gtk_New (Life_Value);
+   --Gtk.Main.Main;
 end;
